ef447aea30b10d9312d0913af8045fd74ea0507e
Ansarhansi <161307300+Ansarhansi@users.noreply.github.com>
2024-09-25 12:47:53 +0530

    Add files via upload

---
+++ p03.js
@@ -0,0 +1,32 @@
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
+document.querySelectorAll('.project-item, .skill-item').forEach(item => {
+    item.classList.add('reveal');
+});
